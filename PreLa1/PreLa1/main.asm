f8a5de655fcc6a75a3da71a1eca84a64ae06f612
Ervin117 <ervinggg04@gmail.com>
2025-02-09 21:04:46 -0600

    Laboratorio completo

--- PreLa1/PreLa1/main.asm
+++ PreLa1/PreLa1/main.asm
@@ -18,7 +18,7 @@ OUT		SPH, R16
 
 //Configuracion de inicio 
 SETUP: 
-	//Configuracion de los pines de entrad y salida
+	//Configuracion de los pines de entrada y salida
 	LDI		R16, 0x0F
 	OUT		DDRB, R16 //PORTB como salida
 	LDI		R16, 0x0F
@@ -27,18 +27,20 @@ SETUP:
 	OUT		DDRD, R16 //PORTD como entrada
 	LDI		R16, 0x0C
 	CLR		R17
-	OUT		PORTB, R17 //Inicio del contador en 0 
+	OUT		PORTB, R17 //Inicio del contador1 en 0 
+	CLR		R18
+	OUT		PORTC, R18 //Inicio del contador2 en 0
 
 MAIN: 
 	//Loop infinito
 	SBIC	PIND, PD2 //Verifica el estado del boton 1
-	RJMP	INCREMENT //Llama la rutina de incremento
+	CALL	INCREMENT //Llama la rutina de incremento
 	SBIC	PIND, PD3 //Verifica el estado del boton 2
-	RJMP	DECREMENT //Llama la rutina de decremento
+	CALL	DECREMENT //Llama la rutina de decremento
 	SBIC	PIND, PD4 //Verifica el estado del boton 3
-	RJMP	INCREMENT2 //Llama la rutina de incremento
+	CALL	INCREMENT2 //Llama la rutina de incremento
 	SBIC	PIND, PD5 //Verifica el estado del boton 4
-	RJMP	DECREMENT2 //Llama la rutina de decremento
+	CALL	DECREMENT2 //Llama la rutina de decremento
 	RJMP	MAIN //Regresa al loop
 
 INCREMENT: 
@@ -48,6 +50,7 @@ INCREMENT:
 	INC		R17		  //Incrementa el contador 
 	OUT		PORTB, R17 //Actualiza la salida
 	RJMP	RETARDO
+	RET
 
 DECREMENT:
 	//Logica de decremento
@@ -56,12 +59,31 @@ DECREMENT:
 	DEC		R17		  //Decrementar el contador 
 	OUT		PORTB, R17 //Actualiza salida
 	RJMP	RETARDO
+	RET
+
+INCREMENT2: 
+	//Logica de incremento 
+	CPI		R18, 0x0F //Para el incremento al llegar al valor 0x0F
+	BREQ	MAIN
+	INC		R18		  //Incrementa el contador 
+	OUT		PORTC, R18 //Actualiza la salida
+	RJMP	RETARDO
+	RET
+
+DECREMENT2:
+	//Logica de decremento
+	CPI		R18, 0x00 //Para el contador si es 0x00
+	BREQ	MAIN
+	DEC		R18 	  //Decrementar el contador 
+	OUT		PORTC, R18 //Actualiza salida
+	RJMP	RETARDO
+	RET
 
 //Logica Antirebotes
 RETARDO: 
-	LDI		R18, 0xFF //Cargar valor para retardo 
+	LDI		R19, 0xFF //Cargar valor para retardo 
 
 DELAY: 
-	DEC		R18
-	BRNE	DELAY 
+	DEC		R19
+	BRNE	DELAY //Espera hasta que R19 llegue a 0. 
 	RJMP	MAIN
